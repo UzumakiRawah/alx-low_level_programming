8cda8ec9a10c7193307f80c42ddb671d6508a9d0
Rawah <113656545+UzumakiRawah@users.noreply.github.com>
2022-10-19 03:54:46 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,18 @@
-nclude "main.h"
+#include "main.h"
 /**
-* main - prints _putchar, fllowd by a new linees
-* Return: Alwys 0 (Success)
-*/
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
+ */
 int main(void)
 {
-char abe[] = "_putchar";
-int c;
-for (c = 0; c < 8; c++)
-{
-_putchar(abe[c]);
-}
-_putchar('\n');
-return (0);
+	char abe[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(abe[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
