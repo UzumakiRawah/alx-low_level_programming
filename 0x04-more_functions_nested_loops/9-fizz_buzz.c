947e3df340457a58b79ffd50ee13b9fb2e62e705
Rawah <113656545+UzumakiRawah@users.noreply.github.com>
2022-10-20 16:32:23 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
+ * main - Fizz for multiples of 3 and Buzz for multiples of 5 FizzBuzz for both
  *
- *  * _isdigit - Check for a digit (0-9)
  *
- *   * @c: Integer holding the character
- *
- *    *
- *
- *     * Return: 1- Digit 0- Not
- *
- *      */
-
-
-
-int _isdigit(int c)
-
+ * Return: Always 0
+ */
+int main(void)
 {
+	int x = 1;
 
-		if (c >= 48 && c <= 57)
+	while (x < 101)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
 
-					return (1);
-
-
-
-			return (0);
-
+		if (x != 100)
+		{
+		printf(" ");
+		}
+		x++;
+	}
+	printf("\n");
+	return (0);
 }
