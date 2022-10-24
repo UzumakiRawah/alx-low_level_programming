ade001c64841d9c533a87cc5611f372356f559e9
UzumakiRawah <rawahaabdo721@gmail.com>
2022-10-24 16:05:10 -0700

    Tasks

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 55be03446fd8297c2947b240f8de08c1f74e0da8
 #include "main.h"
 
-
-
 /**
- *
- *  * reset_to_98 - updates the value *n points to.
- * *   * @n: the number to modify.*
+ * reset_to_98 - updates the value *n points to.
+ * @n: the number to modify.*
  */
 void reset_to_98(int *n)
 {
