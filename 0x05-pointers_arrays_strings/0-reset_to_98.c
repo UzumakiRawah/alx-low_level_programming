3ad83daf2e9c2bd2b16ef86d6249005fd15ed98a
Rawah <113656545+UzumakiRawah@users.noreply.github.com>
2022-10-25 02:46:32 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
-nclude "main.h"
-/**
- 
- * reset_to_98 - updates the value *n points to.
- 
- * @n: the number to modify.
- 
- */
-void reset_to_98(int *n)
-{
-		*n = 98;
-}
-=======
 
-include "main.h"
+#include "main.h"
 
 /**
  * reset_to_98 - updates the value *n points to.
@@ -24,4 +10,3 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
->>>>>>> 8624096c07cc63d28b6d25a3d044bd2a6e00571a
