d5e7021314498e953f7956ffb79e3d52fa26fad1
UzumakiRawah <rawahaabdo721@gmail.com>
2022-10-24 16:42:53 -0700

    Task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-#include "main.h"
-
+nclude "main.h"
 /**
+ 
  * reset_to_98 - updates the value *n points to.
+ 
  * @n: the number to modify.
+ 
  */
 void reset_to_98(int *n)
 {
-	*n = 98;
+		*n = 98;
 }
-=======
-#include "main.h"
-
-/**
- * reset_to_98 - updates the value *n points to.
- * @n: the number to modify.
- */
-
-void reset_to_98(int *n)
-{
-	*n = 98;
-}
->>>>>>> 75095e4402872685bf1e381e014a67bc73a7d36a
