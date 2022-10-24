c73730e484d7611cbab14e38d63186cd691c1b65
Rawah <113656545+UzumakiRawah@users.noreply.github.com>
2022-10-25 02:16:54 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
-void _putchar(char);
+#ifndef MAN_H
+#define MAN_H
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +12,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif
