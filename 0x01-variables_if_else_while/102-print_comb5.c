f241a33cb0df0a053b2c89cb67ce3735c22d0583
Rawah <113656545+UzumakiRawah@users.noreply.github.com>
2022-10-25 03:27:02 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,66 +1,33 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - prints all possible different combinations of three digits
- *
- *   * Return: Always 0 (Success)
- *
- *    */
-
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	int i, j;
 
-		int n, m, l;
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 
-
-
-			for (n = 48; n < 58; n++)
-
-					{
-
-								for (m = 49; m < 58; m++)
-
-											{
-
-															for (l = 50; l < 58; l++)
-
-																			{
-
-																								if (l > m && m > n)
-
-																													{
-
-																																			putchar(n);
-
-																																								putchar(m);
-
-																																													putchar(l);
-
-																																																		if (n != 55 || m != 56)
-
-																																																								{
-
-																																																															putchar(',');
-
-																																																																					putchar(' ');
-
-																																																																										}
-
-																																																						}
-
-																											}
-
-																	}
-
-									}
-
-				putchar('\n');
-
-					return (0);
-
-i
+		}
+	}
+	putchar('\n');
+	return (0);
 }
