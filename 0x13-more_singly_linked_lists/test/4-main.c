fb2183f6db670f46e9e10880e0f240e33cd4b421
Rawah <113656545+UzumakiRawah@users.noreply.github.com>
2022-11-29 18:34:59 +0300

    Update 4-main.c

--- 0x13-more_singly_linked_lists/test/4-main.c
+++ 0x13-more_singly_linked_lists/test/4-main.c
@@ -10,17 +10,19 @@
  */
 int main(void)
 {
-    list_t *head;
+    listint_t *head;
 
     head = NULL;
-    add_node_end(&head, "Bob");
-    add_node_end(&head, "&");
-    add_node_end(&head, "Kris");
-    add_node_end(&head, "love");
-    add_node_end(&head, "asm");
-    print_list(head);
-    free_list(head);
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    free_listint(head);
     head = NULL;
     return (0);
 }
-
